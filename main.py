b14780a3621ecf3793a119139101b9b4850d2822
Guillaume Giroux <45839232+Guillaume2509@users.noreply.github.com>
2021-01-09 22:17:37 -0500

    cherché le tutoriel, docstrings faits, à suivre...
    09-01-2021

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style="darkgrid")
 
 
-def Import_data():
-    '''Importer la donnée'''
+def Import_data(x, y):
+    '''Importe la donnée.
+    
+    Keyword arguments:
+    x -- le jeu de données
+    y -- les configurations souhaitées
+    '''
     
     pd.read_csv('data.csv')
    
    
 def CleanFilter_data():
-    '''Nettoyer les données et ne conserver que les observations et variables souhaitées'''
+    '''Nettoye les données et ne conserve que les observations et variables souhaitées.'''
     
     
 def ScoresNZipcode():
-    '''Calculer le score et merger avec le code postal'''
+    '''Calcule le score et merge avec le code postal.'''
+    
     
 def Convert_to_heatmap():
-    '''Convertir le jeu de données en carte de chaleur géographique'''
+    '''Convertit le jeu de données en carte de chaleur géographique.
+    
+    Source: https://medium.com/@m_vemuri/create-a-geographic-heat-map-of-the-city-of-toronto-in-python-cd2ae0f8be55
+    
+    Commencer par Toronto pour le tutoriel'''
+    
+    shape files
     
 
 if __name__ == "__main__":
